725b688d5b07b3b048fc710b5e4ac7f0b394b7e3
John Cunha <johnstrongedge@proton.me>
2025-05-06 11:01:53 +0100

    PWM driving motor from rotary encoder value, rotary button changes motor direction.

---
+++ arduino/motorDriver/motorDriver.ino
@@ -0,0 +1,97 @@
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                            I/O PIN DEFINITIONS
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const int DIR_B = 13;
+const int PWM_B = 11;
+
+const int ENCODER_A = 3;
+const int ENCODER_B = 4;
+
+const int DAT = 2;
+const int CLK = 5;
+const int BUTTON = 6;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                            Global Variables
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool blinking = false;
+bool motor_direction = false;
+int target_time;
+
+bool pressed = false;
+bool lastPressed = false;
+int rotary_count = 0;
+
+long encoder_count = 0;
+int pwm_output = 100;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A), rightEncoderEvent, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DAT), RoteStateChanged, FALLING);
+
+  pinMode(DIR_B, OUTPUT);
+  pinMode(PWM_B, OUTPUT);
+
+  pinMode(ENCODER_B, INPUT);
+  pinMode(CLK, INPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+
+  lastPressed = pressed;
+  pressed = digitalRead(BUTTON);
+  if (lastPressed == false && pressed == true) {
+    Serial.println("Change Direction!!");
+    motor_direction = !motor_direction;
+  }
+
+  if (motor_direction) {
+    digitalWrite(DIR_B, HIGH);
+  } else {
+    digitalWrite(DIR_B, LOW);
+  }
+
+  pwm_output = min(100, max(0, rotary_count));
+  pwm_output = map(pwm_output, 0, 100, 0, 255);
+  analogWrite(PWM_B, pwm_output);
+  
+  Serial.print("encoder count: ");
+  Serial.println(encoder_count);  
+  Serial.print("rotary count: ");
+  Serial.println(rotary_count);  
+  delay (100);
+
+
+}
+
+void rightEncoderEvent() {
+  if (digitalRead(ENCODER_A) == HIGH) {
+    if (digitalRead(ENCODER_B) == LOW) {
+      encoder_count++; }
+    else {
+      encoder_count--; }
+  } else {
+    if (digitalRead(ENCODER_B) == LOW) {
+      encoder_count--;
+    } else {
+      encoder_count++;
+    }
+  }
+}
+
+
+void RoteStateChanged() {
+  if (digitalRead(CLK) == LOW) {
+    rotary_count++; }
+  else {
+    rotary_count--; }
+
+}
+
